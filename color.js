c5e6085259a05925ee05a11b13a0c75b7e3232fa
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-04-02 00:12:03 +1100

    cannot use the arrow function for some reason, so using classic function

--- color.js
+++ color.js
@@ -2,19 +2,41 @@ var numSquare = 6;
 var square = document.querySelectorAll(".square");
 var color = generateRandomColor(numSquare);
 
-generateRandomColor = (numSquare) => {
-  let arr = [];
+// const generateRandomColor = (num) => {
+//   let arr = [];
 
+//   for (var i = 0; i < num; i++) {
+//     arr.push(ranColors());
+//   }
+//   return arr;
+// };
+
+function generateRandomColor(num) {
+  // make an array
+  var arr = [];
+  // loop num times
   for (var i = 0; i < num; i++) {
+    // get randomColors and push them into the array
+
     arr.push(ranColors());
   }
+  // return array
   return arr;
-};
+}
 
-const ranColors = () => {
+// const ranColors = () => {
+//   var r = Math.floor(Math.random() * 256);
+//   var g = Math.floor(Math.random() * 256);
+//   var b = Math.floor(Math.random() * 256);
+
+//   return "rgb(" + r + ", " + g + ", " + b + ")";
+// };
+
+function ranColors() {
   var r = Math.floor(Math.random() * 256);
   var g = Math.floor(Math.random() * 256);
   var b = Math.floor(Math.random() * 256);
 
   return "rgb(" + r + ", " + g + ", " + b + ")";
-};
+}
+console.log(color);
