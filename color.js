e49cae730f21dfbed52bb76a45ef5773c650ba4e
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-04-01 23:58:48 +1100

    generated rgb color-inital commit

---
+++ color.js
@@ -0,0 +1,13 @@
+var numSquare = 6;
+
+generate;
+
+const ranColors = () => {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+};
+
+console.log(ranColors());
